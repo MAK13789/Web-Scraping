1f27d24d1a2d97f76c18df6c7abfefabcf46058c
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2020-11-12 15:34:29 -0500

    Add files via upload
    some new changes

--- web scraping.py
+++ web scraping.py
@@ -1,6 +1,7 @@
 import urllib
 from urllib import request
 import bs4
+from statistics import mode
 link = "https://www.theatlantic.com/ideas/archive/2020/11/trump-proved-authoritarians-can-get-elected-america/617023/"
 f = urllib.request.urlopen(link)
 soup = bs4.BeautifulSoup(f)
@@ -19,7 +20,14 @@ while j >= 0:
     if temp[j].isalpha() == False and temp[j] != ' ' and temp[j] != "'":
         temp = temp[:j] + temp[j+1:]
     j -= 1
-print (temp)
+new = list(temp.split(' '))
+most_common_list = []
+for a in range(11):
+    most_common_list.append(mode(new))
+    new = [k for k in new if k != mode(new)]
+print (most_common_list)
+#USE TRY EXCEPT OR STH fOR WHEN THE MODE ISNT UNIQUE
+#MAKE THESE STUFF INTO FUNCTIONS SO THAT IT IS EASIER WHEN MULTIPLE ARTICLES ARE USED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #find a better way to split this so that it always starst from the beginning of the text and ends at the end of the text 
 #(for example, finding where the title or date is)
 #for now, just split it at some arbritrary indices that should work for most articles
