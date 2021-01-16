197da4c80aef3e304facc7aaf8c8628ee89047b4
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-01-16 23:29:14 +0500

    added testing code and notes file

---
+++ testing.py
@@ -0,0 +1,3 @@
+from main import most_common_words
+link = "https://www.theatlantic.com/ideas/archive/2020/11/trump-proved-authoritarians-can-get-elected-america/617023/"
+print (most_common_words(link))
